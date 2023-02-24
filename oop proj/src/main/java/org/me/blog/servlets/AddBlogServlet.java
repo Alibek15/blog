09da1b87b382c072abd7f85c8e5e676e62308031
Sabina <121316363+Sakass05@users.noreply.github.com>
2023-02-24 11:32:54 +0600

    Update AddBlogServlet.java

--- oop proj/src/main/java/org/me/blog/servlets/AddBlogServlet.java
+++ oop proj/src/main/java/org/me/blog/servlets/AddBlogServlet.java
@@ -5,15 +5,17 @@ import javax.servlet.http.*;
 import javax.servlet.annotation.*;
 import java.io.IOException;
 
-@WebServlet(name = "InfoServlet", value = "/InfoServlet")
-public class InfoServlet extends HttpServlet {
+@WebServlet( value = "/addblog")
+public class AddBlogServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        User user = (User) request.getSession().getAttribute("USER_SESSION");
+
+        if(user != null){
+            request.getRequestDispatcher("addblog.jsp").forward(request,response);
+        }
+        else{
+            response.sendRedirect("/login");
+        }
 
     }
-
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
-    }
-}
