53cefa9b5cd3a5c3ddd49d7bd9436a4d8b4ed6a0
Rakhat2131 <115026141+Rakhat2131@users.noreply.github.com>
2023-02-24 11:45:38 +0600

    Update ShowBlogServlet.java

--- oop proj/src/main/java/org/me/blog/servlets/ShowBlogServlet.java
+++ oop proj/src/main/java/org/me/blog/servlets/ShowBlogServlet.java
@@ -1,19 +1,27 @@
 package org.me.blog.servlets;
 
+import lombok.extern.java.Log;
+import lombok.extern.slf4j.Slf4j;
+import org.me.blog.db.DbManager;
+import org.me.blog.db.Operations;
+import org.me.blog.entity.Post;
+import org.me.blog.entity.User;
+import org.me.blog.model.Blog;
+
 import javax.servlet.*;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
 import java.io.IOException;
-
-@WebServlet(name = "InfoServlet", value = "/InfoServlet")
-public class InfoServlet extends HttpServlet {
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+@Log
+@WebServlet(value = "/show")
+public class ShowBlogServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
-    }
-
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+        request.getRequestDispatcher("show.jsp").forward(request,response);
     }
 }
