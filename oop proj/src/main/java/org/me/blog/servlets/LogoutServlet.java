9d4ac0bacf95d7c10edd04e3c708f2427b904550
KussainovAnsar <119420509+KussainovAnsar@users.noreply.github.com>
2023-02-25 10:16:14 +0600

    Create LogoutServlet.java

---
+++ oop proj/src/main/java/org/me/blog/servlets/LogoutServlet.java
@@ -0,0 +1,22 @@
+package org.me.blog.servlets;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+
+@WebServlet(value = "/logout")
+public class LogoutServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        HttpSession session = request.getSession();
+        session.removeAttribute("USER_SESSION");
+        request.getRequestDispatcher("login.jsp").forward(request,response);
+        response.sendRedirect("/");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
